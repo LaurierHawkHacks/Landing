997640da2e61040840f006dc74713dd58d961833
ObiTracks <straightup.stack@gmail.com>
2022-04-04 10:37:38 -0400

    MeetTheTeam section basic html created. Fleshing out the inner components

--- src/App.js
+++ src/App.js
@@ -7,28 +7,14 @@ import SocialIcons from "./components/SocialIcons";
 import FAQ from "./components/FAQ";
 import SponsorTiersDisplay from "./components/sponsors/SponsorTiersDisplay";
 import SponsorCarousel from "./components/sponsors/SponsorCarousel";
+import MeetTheTeam from "./components/meet_the_team/MeetTheTeam";
 import Icon from "./assets/icon.svg";
-import SponsorHawk from "./assets/sponsor_hawk_heart.png";
+import SponsorHawk from "./assets/sponsor_hawk.svg";
 import AboutIllustration from "./assets/illustration_about.svg";
 
 import "./App.css";
-import PartnersSection from "./components/partners/PartnersSection";
 
 const GradientBackground = Styled.div`
-`;
-
-const HeroButtonPrimary = Styled(Button)`
-    color: white;
-    background-color: #0fa3b1;
-    border-width: 0;
-    font-weight: 600;
-`;
-
-const HeroButtonSecondary = Styled(Button)`
-    color: #2f4858;
-    background-color: #eddadd;
-    border-width: 0;
-    font-weight: 600;
 `;
 
 function App() {
@@ -39,6 +25,7 @@ function App() {
                     <Illustration src_name="illustration_1" top="2em" />
                     <Illustration src_name="illustration_2" top="40em" />
                     <Illustration src_name="illustration_3" top="110em" />
+
                     <Navbar id="header" expand="lg" sticky="top" className="bg-hh-accent">
                         <Container className="mt-4 mb-3">
                             <Navbar.Brand href="#">
@@ -56,14 +43,14 @@ function App() {
                                     <Nav.Link href="#about" className="mx-1">
                                         About
                                     </Nav.Link>
+                                    <Nav.Link href="#why-laurier" className="mx-1">
+                                        Why us? (rename?)
+                                    </Nav.Link>
                                     <Nav.Link href="#faq" className="mx-1">
                                         FAQ
                                     </Nav.Link>
                                     <Nav.Link href="#newsletter" className="mx-1">
                                         Newsletter
-                                    </Nav.Link>
-                                    <Nav.Link href="#partners" className="mx-1">
-                                        Partners
                                     </Nav.Link>
                                     <Nav.Link href="#sponsors" className="mx-1">
                                         Sponsors
@@ -94,45 +81,32 @@ function App() {
                             height="110"
                             alt="HawkHacks Icon"
                         />
-                        <h1 className="font-weight-bold">&lt;HawkHacks 2022/&gt;</h1>
-                        <h5>
-                            <b>Wilfrid Laurier’s first large-scale global hackathon!</b>
-                        </h5>
-                        <div className="spacer" style={{ width: "100%", height: "2em" }} />
-                        <p>
-                            Wilfrid Laurier University | Fully Virtual | May 13<sup>th</sup> - 15
-                            <sup>th</sup>
-                        </p>
-                        <div
-                            id="home-buttons"
-                            className="d-flex flex-column flex-sm-row w-100 px-4 justify-content-center"
-                        >
-                            <HeroButtonSecondary href="#sponsors" className="my-2" size="sm">
-                                Sponsor Us
-                            </HeroButtonSecondary>
-                            <HeroButtonPrimary href="https://www.google.com/" className="px-5" size="sm">
-                                Join Now!
-                            </HeroButtonPrimary>
-                            <HeroButtonSecondary href="#sponsors" className="my-2" size="sm">
-                                Become a Mentor!
-                            </HeroButtonSecondary>
+                        <h1 className="font-weight-bold">&lt;HawkHacks/&gt;</h1>
+                        <h1 className="font-weight-bold">2022</h1>
+                        <h5>put some fun slogan here</h5>
+                        <p>Laurier | On-Campus and Virtual</p>
+                        <div id="home-buttons" className="d-flex flex-column flex-sm-row w-100 px-4 justify-content-center">
+                            <Button className="d-flex flex-grow-1 flex-sm-grow-0 justify-content-center my-2 py-2">Apply</Button>
+                            <Button className="d-flex flex-grow-1 flex-sm-grow-0 justify-content-center my-2 py-2">Sponsor</Button>
+                            <Button className="d-flex flex-grow-1 flex-sm-grow-0 justify-content-center my-2 py-2">Events</Button>
                         </div>
                     </Container>
 
                     <Container id="about" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "10em" }} />
+
+
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <div className="row">
                             <div className="col-md-6">
                                 <h2 className="font-weight-bold">&lt;About HawkHacks/&gt;</h2>
                                 <p>
                                     HawkHacks came out of a desire to give everyone an equal
                                     opportunity to get into tech, whether that be programming,
-                                    networking, researching, learning, or teaching.
+                                    networking, researching, learning, or teaching.<br />
                                     <br />
-                                    <br />
-                                    Join hundreds of students across Canada (and across the world)
-                                    in a 36 hour period of exploration, creativity, and learning!
-                                    <br />
+                                    Join hundreds of students across Canada (and across the
+                                    world) in a 36 hour period of exploration, creativity, and
+                                    learning!<br />
                                     <br />
                                     Remember, you don't have to be a pro to participate - show up
                                     with ten years or ten minutes of experience (oh yeah, and a
@@ -141,12 +115,7 @@ function App() {
                             </div>
 
                             <div className="col-md-6 text-right">
-                                <img
-                                    src={AboutIllustration}
-                                    className="img-fluid"
-                                    alt="About Illustration"
-                                    style={{ opacity: "1.0" }}
-                                />
+                                <img src={AboutIllustration} className="img-fluid" alt="About Illustration" style={{ opacity: "1.0" }} />
                             </div>
 
                             <div className="col" />
@@ -154,83 +123,80 @@ function App() {
                     </Container>
 
                     <Container id="why-laurier" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "7.5em" }} />
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <div className="row">
                             <div className="pe-lg-5" />
                             <div className="col-md-6 text-center">
+                                {/*eslint-disable-next-line*/}
                                 <h3>// Why Laurier?</h3>
                             </div>
                             <div className="col-md-5 text-left">
                                 <p>
-                                    Our goal with HawkHacks is to showcase the amazing talent on
-                                    display at our university. Often, smaller universities are
-                                    outshun by bigger ones - we’d like to change that, by showing
-                                    how awesome of a community exists here too!
+                                    Our goal with HawkHacks is to showcase the amazing talent on display at our university.
+                                    Often, smaller universities are outshun by bigger ones - we’d like to change that,
+                                    by showing how awesome of a community exists here too!
                                 </p>
                             </div>
                         </div>
 
-                        <div className="spacer" style={{ width: "100%", height: "7.5em" }} />
-
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <div className="row">
                             <div className="col-md-12 text-center">
+                                {/*eslint-disable-next-line*/}
                                 <h3>// What can I gain?</h3>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-md-12 text-center">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in
-                                    ligula ut nisi volutpat mollis. Mauris in augue quis augue
-                                    dignissim ullamcorper non porttitor nulla. Nam dapibus urna eget
-                                    sagittis suscipit. Sed quis ipsum libero. Aliquam non tempor
-                                    enim.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Nam in ligula ut nisi volutpat mollis.
+                                    Mauris in augue quis augue dignissim ullamcorper non porttitor nulla.
+                                    Nam dapibus urna eget sagittis suscipit.
+                                    Sed quis ipsum libero. Aliquam non tempor enim.
                                 </p>
                             </div>
                         </div>
+
                     </Container>
 
                     <Container id="faq" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "10em" }} />
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <FAQ />
                     </Container>
 
                     <Container id="newsletter" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "10em" }} />
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <div className="row">
                             <div className="col-md-6 text-left">
+                                {/*eslint-disable-next-line*/}
                                 <h3>// Want to stay updated?</h3>
-                                <p className="mb-0">
-                                    Reach out at{" "}
-                                    <a href="mailto:hello@hawkhacks.ca">hello@hawkhacks.ca</a> for
-                                    any help or support!
+                                <p>
+                                    Reach out at <a href="mailto:hello@hawkhacks.ca">hello@hawkhacks.ca</a> for any help or support!
+                                    <SocialIcons></SocialIcons>
                                 </p>
-                                <SocialIcons />
-                                <InputGroup className="mb-3 mt-3">
+
+                                <InputGroup className="mb-3">
                                     <FormControl
                                         placeholder="Enter your email to subscribe to our newsletter!"
                                         aria-label="Recipient's email"
                                         aria-describedby="basic-addon2"
                                     />
 
-                                    <Button>{">>"}</Button>
+                                    <Button>
+                                        {">>"}
+                                    </Button>
                                 </InputGroup>
                             </div>
                         </div>
                     </Container>
 
-                    <Container id="partners">
-                        <div className="spacer" style={{ width: "100%", height: "7.5em" }} />
-                        <PartnersSection />
-                    </Container>
-
                     <Container id="sponsors" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "7.5em" }} />
+                        <div className="spacer" style={{ width: "100%", height: "13em" }} />
                         <div className="our-sponsors">
                             <h2 className="sponsors-title">{"<Our Sponsors/>"}</h2>
                             <p className="sponsors-subtitle">
-                                These are the people who make what we do possible. Check out all our
-                                amazing sponsors below, and tell them we sent you!
+                                These are the people who make what we do possible. Check out all our amazing sponsors below, and tell them we sent you!
                             </p>
                             <img src={SponsorHawk} alt="sponsor bird" className="sponsors-bird" />
                         </div>
@@ -238,31 +204,22 @@ function App() {
                         <SponsorTiersDisplay />
                     </Container>
 
-                    <Container id="team" className="foreground">
-                        <div className="spacer" style={{ width: "100%", height: "7.5em" }} />
+                    <Container id="meettheteam" className="foreground">
+                        <MeetTheTeam />
                     </Container>
+
                     {/* <div className="spacer" style={{ width: '100%', height: '13em' }} />
                 <div className="spacer" style={{ width: '100%', height: '30em' }} /> */}
                 </div>
 
                 <div>
-                    <Illustration
-                        src_name="illustration_4"
-                        width="100%"
-                        maxWidth="102em"
-                        position="relative"
-                        id="illustration-footer"
-                    />
-                    <div
-                        id="footer"
-                        className="foreground site-footer m-0 w-100 d-flex justify-content-center"
-                    >
+                    <Illustration src_name="illustration_4" width="100%" maxWidth="102em" position="relative" id="illustration-footer" />
+                    <div id="footer" className="foreground site-footer m-0 w-100 d-flex justify-content-center">
                         <Row className="site-footer-content d-flex justify-content-center align-items-center">
                             <a
                                 className="my-1 w-auto"
                                 href={"https://static.mlh.io/docs/mlh-code-of-conduct.pdf"}
-                                target="_blank"
-                                rel="noreferrer"
+                                target="_blank" rel="noreferrer"
                                 style={{ textDecoration: "none", color: "white" }}
                             >
                                 Code of Conduct
---
+++ src/components/meet_the_team/MeetTheTeam.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+
+const MeetTheTeam = () => {
+    return (
+
+        <div>
+            <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
+                <h2 className="font-weight-bold text-center">Meet Deez Nuts</h2>
+                <div>sdfsdfs</div>
+                <div className="teams-container">
+                    <div className="team">
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                    </div>
+                    <div className="team">
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                    </div>
+                    <div className="team">
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                    </div>
+                    <div className="team">
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                    </div>
+                    <div className="team">
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                        <div className="team-member"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+
+}
+
+export default MeetTheTeam;
