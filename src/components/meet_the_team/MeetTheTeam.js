26d69c2810a5ba649d62b44ba06b2b3c4f5d4175
ObiTracks <straightup.stack@gmail.com>
2022-04-04 10:39:23 -0400

    Update

--- src/components/meet_the_team/MeetTheTeam.js
+++ src/components/meet_the_team/MeetTheTeam.js
@@ -6,29 +6,38 @@ const MeetTheTeam = () => {
 
         <div>
             <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
-                <h2 className="font-weight-bold text-center">Meet Deez Nuts</h2>
-                <div>sdfsdfs</div>
+                <h2 className="font-weight-bold text-center">Meet The Team</h2>
+                <div>Subtitle</div>
                 <div className="teams-container">
+                    <div className="team-name"></div>
                     <div className="team">
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                     </div>
+
+                    <div className="team-name"></div>
                     <div className="team">
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                     </div>
+
+                    <div className="team-name"></div>
                     <div className="team">
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                     </div>
+
+                    <div className="team-name"></div>
                     <div className="team">
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                         <div className="team-member"></div>
                     </div>
+
+                    <div className="team-name"></div>
                     <div className="team">
                         <div className="team-member"></div>
                         <div className="team-member"></div>
