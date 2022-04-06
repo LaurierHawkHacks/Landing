035b19d7680e2ca46aaaeeaafa406e30de59a85d
ObiTracks <straightup.stack@gmail.com>
2022-04-06 16:16:48 -0400

    Meet the team grid and basic dropdown for member cards working. Needs improvements. Next steps are to finish the css then configure the JS to load the team members, their information and links into cards from JSON  (will need to turn the team information doc into JSON)

--- src/components/meet_the_team/MeetTheTeam.js
+++ src/components/meet_the_team/MeetTheTeam.js
@@ -1,4 +1,27 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+import TeamMember from "./TeamMember";
+
+
+    const TeamsContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background: pink;
+`;
+
+const TeamName = styled.div`
+width: 100%;
+    font-size: 20px;
+  color: red;
+`;
+
+const Team = styled.div`
+display: grid;
+grid-gap: 30px;
+grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+background: blue;
+`;
 
 
 const MeetTheTeam = () => {
@@ -8,45 +31,45 @@ const MeetTheTeam = () => {
             <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
                 <h2 className="font-weight-bold text-center">Meet The Team</h2>
                 <div>Subtitle</div>
-                <div className="teams-container">
-                    <div className="team-name"></div>
-                    <div className="team">
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                    </div>
-
-                    <div className="team-name"></div>
-                    <div className="team">
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                    </div>
-
-                    <div className="team-name"></div>
-                    <div className="team">
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                    </div>
-
-                    <div className="team-name"></div>
-                    <div className="team">
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                    </div>
-
-                    <div className="team-name"></div>
-                    <div className="team">
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                        <div className="team-member"></div>
-                    </div>
-                </div>
-
+                <TeamsContainer>
+                    <TeamName> Development Team </TeamName>
+                    <Team>
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                        <TeamMember name="GongKong" />
+                    </Team>
+                </TeamsContainer>
             </div>
         </div>
+        // <div>
+        //     <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
+        //         <h2 className="font-weight-bold text-center">Meet The Team</h2>
+        //         <div>Subtitle</div>
+        //         <div className="teams-container">
+        //             <div className="team-name"></div>
+        //             <div className="team">
+        //                 <TeamMember />
+        //                 <div className="team-member"></div>
+        //                 <div className="team-member"></div>
+        //                 <div className="team-member"></div>
+        //             </div>
+
+        //             <div className="team-name"></div>
+        //             <div className="team">
+        //                 <div className="team-member"></div>
+        //                 <div className="team-member"></div>
+        //                 <div className="team-member"></div>
+        //             </div>
+
+        //         </div>
+
+        //     </div>
+        // </div>
     );
 
 }
--- src/components/meet_the_team/TeamMember.js
+++ src/components/meet_the_team/TeamMember.js
@@ -1,31 +1,160 @@
 import React from "react";
 import Styled from "styled-components";
 
+import styled, { keyframes } from "styled-components";
 
-const MemberCardContainer = Styled.div`
-
+const dropdownAnimationDown = keyframes`
+  0% { transform: translateY(0%); } /* slide the dropdown down */
+  100% { transform: translateY(30%); }
 `;
 
-const MemberCardImage = Styled.div`
-border
-`;
-
-const MemberCardContent = Styled.p1`
-
+const dropdownAnimationUp = keyframes`
+  0% { transform: translateY(30%); } /* slide the dropdown up */
+  100% { transform: translateY(0%); }
 `;
 
 
-function TeamMember({ name, position, image, bio, linkedin }) {
 
-    return(
-        <MemberCardContainer>
-            <MemberCardImage src={image}/>
-            <MemberCardContent>
-                <p>{name}</p>
-                <p>{position}</p>
-            </MemberCardContent>
-        </MemberCardContainer>
+// const CardDiv = styled.div`
+//   width: 300px;
+
+//   :hover {
+//     cursor: pointer;
+//     ${ButtonDiv} {
+//       animation-name: ${dropdownAnimationDown};
+//       animation-duration: 0.3s;
+//       animation-fill-mode: forwards;
+//     }
+//   }
+// `;
+
+
+// const Card = styled.div`
+//   margin-top: 1em;
+//   background-color: blue;
+//   height: 80px;
+// `;
+
+// const ButtonDiv = styled.div`
+//   margin-top: 1em; /* added margin-top to both the card and button so they would line-up*/
+
+//   position: absolute; /* allows the button div to sit behind the card */
+//   top: 0px;
+//   z-index: -1; /* allows the button div to sit behind the card */
+
+//   height: 80px;
+//   width: inherit;
+//   background-color: red;
+
+//   animation-name: ${dropdownAnimationUp};
+//   animation-duration: 0.3s;
+//   animation-fill-mode: forwards;
+// `;
+
+const Wrapper = styled.div`
+    position: relative;
+    overflow: hidden;
+    
+    // width: fit-content;
+    height: fit-content;
+    background: red;
+    
+    
+`;
+
+const Card = styled.div`
+    position: relative;
+    overflow: hidden;
+    
+    width: 300px;
+    height: fit-content;
+    max-height: 100px;
+    top: 0;
+    
+    display: flex;
+    flex-direction: row;
+    /*   background: red; */
+    z-index: 1;
+`;
+
+const Image = styled.div`
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    border: solid black 2px;
+    background: grey;
+    margin-right: 10px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    flex: 1;
+`;
+
+const Name = styled.div`
+    width: 100%;
+`;
+
+const Role = styled.div`
+    width: 100%;
+`;
+
+const Description = styled.div`
+    position: relative;
+    max-height: 0;
+    overflow: hidden;
+    width: 100%;
+    
+    background: yellow;
+    z-index: -1;
+
+        -moz-transition: top 1s;
+        -o-transition: top 1s;
+        transition: top 1s;
+
+    // transition: top 1s ease-out; 
+    transition: max-height 1s ease-out;
+
+    ${Wrapper}:hover &{
+        max-height: 500px;
+        transition: max-height 1s ease-in;
+    }
+`;
+
+
+
+
+export default function TeamMember(props) {
+    return (
+        // <CardDiv>
+        //     <Card>{props.name}</Card>
+        //     <ButtonDiv></ButtonDiv>
+        // </CardDiv>
+        <Wrapper>
+            <Card>
+                <Image></Image>
+                <Header>
+                    <Name>{props.name}</Name>
+                    <Role>Role 1</Role>
+                </Header>
+            </Card>
+            <Description>Lorem ipsum dipsum</Description>
+        </Wrapper>
+
+        /* <div className="wrapper">
+        <div className="card"> 
+          <img className="card-image" src="" alt=""></img>
+          <div className="card-header">
+              <div className="class-header__name">Person Name</div>
+              <div className="class-header__roles">Position 1</div>
+          </div>
+        </div>
+        
+        <div className="card-hover-description">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</div>
+        
+        </div> */
     );
-} 
+}
 
-export default TeamMember;
