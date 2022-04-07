73bf765911b2e93bf220742422f75291c6f03fff
ObiTracks <straightup.stack@gmail.com>
2022-04-07 19:25:44 -0400

    Reformatted json

--- src/components/meet_the_team/MeetTheTeam.js
+++ src/components/meet_the_team/MeetTheTeam.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import TeamMember from "./TeamMember";
+import teamBios from "../../teamBios_grouped.json";
 
 
 const TeamsContainer = styled.div`
@@ -24,19 +25,18 @@ const Team = styled.div`
 
 
 const MeetTheTeam = () => {
+    console.log("GEllo");
     return (
-
         <div>
+
             <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
                 <h2 className="font-weight-bold text-center">Meet The Team</h2>
                 <div>Subtitle</div>
                 <TeamsContainer>
+                    {teamBios.map((team, members))}
+
                     <TeamName> Development Team </TeamName>
                     <Team>
-                        <TeamMember name="GongKong" />
-                        <TeamMember name="GongKong" />
-                        <TeamMember name="GongKong" />
-                        <TeamMember name="GongKong" />
                         <TeamMember name="GongKong" />
                         <TeamMember name="GongKong" />
                         <TeamMember name="GongKong" />
@@ -45,30 +45,6 @@ const MeetTheTeam = () => {
                 </TeamsContainer>
             </div>
         </div>
-        // <div>
-        //     <div className="col-md-9 col-lg-8 col-xl-7 mx-auto">
-        //         <h2 className="font-weight-bold text-center">Meet The Team</h2>
-        //         <div>Subtitle</div>
-        //         <div className="teams-container">
-        //             <div className="team-name"></div>
-        //             <div className="team">
-        //                 <TeamMember />
-        //                 <div className="team-member"></div>
-        //                 <div className="team-member"></div>
-        //                 <div className="team-member"></div>
-        //             </div>
-
-        //             <div className="team-name"></div>
-        //             <div className="team">
-        //                 <div className="team-member"></div>
-        //                 <div className="team-member"></div>
-        //                 <div className="team-member"></div>
-        //             </div>
-
-        //         </div>
-
-        //     </div>
-        // </div>
     );
 
 }
