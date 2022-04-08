6aea004be26e29dc7b6f7e64f7d7979fafeadc37
Nausher Rao <SherRao15@gmail.com>
2022-04-08 15:07:12 -0400

    Add sponsor logos

--- src/assets/sponsors/index.jsx
+++ src/assets/sponsors/index.jsx
@@ -1,12 +1,18 @@
 // NOTE: Must match the value in "sponsorData.json"
 import DCL from "./dcl.png";
 import Taskade from "./taskade.svg";
-import DigitalOceanHorizontal from "./digitalocean_horizontal.svg";
-import DigitalOceanVertical from "./digitalocean_vertical.svg";
+import DigitalOcean from "./digitalocean.svg";
 import OnePassword from "./1password.svg";
 import Balsamiq from "./balsamiq.png";
 import Echo3D from "./echo3D.png";
 
-// import Linode from "./linode.png";
+import TempSOC from "./temp/soc.png";
+import TempMoneyClip from "./temp/moneyclip.svg";
+import TempLeadingLearners from "./temp/leadinglearners.png";
+import TempDhari from "./temp/dhari.png";
+import TempWolfram from "./temp/wolfram.png";
+import TempGFG from "./temp/gfg.png";
+import TempLinode from "./temp/linode.svg";
+import TempCisco from "./temp/cisco.png";
 
-export { DCL, Taskade, DigitalOceanHorizontal, DigitalOceanVertical, OnePassword, Balsamiq, Echo3D };
+export { DCL, Taskade, DigitalOcean, OnePassword, Balsamiq, Echo3D, TempSOC, TempMoneyClip, TempLeadingLearners, TempDhari, TempWolfram, TempGFG, TempLinode, TempCisco };
