f0d91033d09b63b700108d9a313aa16bc28bd5f9
Nausher Rao <SherRao15@gmail.com>
2022-05-12 18:21:54 -0400

    Add sticker sponsor

--- src/assets/partners/index.jsx
+++ src/assets/partners/index.jsx
@@ -3,5 +3,6 @@ import LCS from "./lcs.svg";
 import MLH from "./mlh.svg";
 import SU from "./su.svg";
 import FOSSA from "./fossa.png";
+import StandOutStickers from "./standoutstickers.png";
 
-export { C3, LCS, MLH, SU, FOSSA };
+export { C3, LCS, MLH, SU, FOSSA, StandOutStickers };
