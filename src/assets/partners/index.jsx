ebc53fa5716b2bcf7deca99c47e57e43f5991b0f
Aidan Traboulay <trabsy@Aidans-MBP.wlu.ca>
2023-11-06 19:29:47 -0500

    Purge old main

--- src/assets/partners/index.jsx
+++
@@ -1,8 +0,0 @@
-import C3 from "./c3.svg";
-import LCS from "./lcs.svg";
-import MLH from "./mlh.svg";
-import SU from "./su.svg";
-import FOSSA from "./fossa.png";
-import StandOutStickers from "./standoutstickers.png";
-
-export { C3, LCS, MLH, SU, FOSSA, StandOutStickers };
