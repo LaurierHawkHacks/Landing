ac3edc1cbecc24b9d68372e6c527b156161d72b2
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-18 00:32:25 -0500

    Set custom colours in tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      base: '#001236',
+      primary: '#523FF4',
+      secondary: '#8375FF',
+      accent: '#12B3C2',
+      text: '#CEDFFF'
+    },
 		extend: {
 			fontFamily: {
 				sans: ['"Raleway"', ...defaultTheme.fontFamily.sans],
