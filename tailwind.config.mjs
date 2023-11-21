c8c6db6929767dd748f741c9d36efa08c1d75c8e
Abdul Basit <abdul.b0227@gmail.com>
2023-11-21 15:38:02 -0500

    fixes Setup Typography #32

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,20 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['"Raleway"', ...defaultTheme.fontFamily.sans],
-            },
-            colors: {
-                "midnight": "#001236",
-                "lilac": "#523FF4",
-                "tahiti": "#8375FF",
-                "mint": "#12B3C2",
-                "lavender": "#CEDFFF",
-            },
-        },
-    },
-    plugins: [],
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Raleway"', ...defaultTheme.fontFamily.sans],
+				serif: ['"Poppins"', ...defaultTheme.fontFamily.serif],
+			},
+			colors: {
+				midnight: "#001236",
+				lilac: "#523FF4",
+				tahiti: "#8375FF",
+				mint: "#12B3C2",
+				lavender: "#CEDFFF",
+			},
+		},
+	},
+	plugins: [],
 };
