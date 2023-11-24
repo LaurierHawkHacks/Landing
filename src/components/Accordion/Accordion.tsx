bf79c1e99781320d8825d72655d0a2e6c3a83bf3
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-24 16:21:20 -0500

    Set the accordion component to full-width

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,7 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 
 const Accordion: FC<PropsWithChildren> = ({ children }) => {
-  return <div className="mx-auto grid max-w-[68.75rem]">{children}</div>;
+  return <div className="w-full">{children}</div>;
 };
 
 export { Accordion };
