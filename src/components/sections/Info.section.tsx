49569732147f38aadc9561ed434b533c1b6a0e4a
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-23 21:02:25 -0500

    Update: add content

--- src/components/sections/Info.section.tsx
+++ src/components/sections/Info.section.tsx
@@ -1,5 +1,34 @@
 const InfoSection = () => {
-  return <></>;
+  return (
+    <>
+      <section>
+        <div>
+          <h1>What is HawkHacks</h1>
+          <p>
+            HawkHacks came out of a desire to give everyone an equal opportunity
+            to get into tech, whether that be programming, networking,
+            researching, learning, or teaching. Join hundreds of students across
+            Canada (and across the world) in a 36 hour period of exploration,
+            creativity, and learning!
+          </p>
+          <p>
+            Remember, you do not have to be a pro to participate - show up with
+            ten years or ten minutes of experience (oh yeah, and a great
+            attitude as well!)
+          </p>
+        </div>
+        <div>
+          <h1>Our Goals</h1>
+          <p>
+            Our goal with HawkHacks is to showcase the amazing talent on display
+            at our university. Often, smaller universities are outshun by bigger
+            ones - we would like to change that, by showing how awesome of a
+            community exists here too!
+          </p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export { InfoSection };
