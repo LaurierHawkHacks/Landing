85324ae3432cf4547d4ed88a3af3a2f26ede0b92
Abdul Basit <abdul.b0227@gmail.com>
2023-11-25 19:52:26 -0500

    push navbar closer, change navbar font size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,14 +21,16 @@ const Navbar = () => {
 	return (
 		<>
 			<nav
-				className={`flex w-full items-center gap-2 bg-midnight text-white sticky top-0 p-4 z-50 px-16 justify-between ${
-					scrollPos > 100 && "shadow-xl"
+				className={`flex w-full items-center gap-2 bg-midnight text-white sticky top-0  z-50 px-24 justify-between ${
+					scrollPos > 100
+						? "shadow-lg p-4 transition-all duration-500 ease-in-out"
+						: "p-8 transition-all duration-500 ease-in-out"
 				} `}
 			>
-				<div className="flex items-center gap-5">
+				<div className="flex items-center gap-8">
 					<img src="./src/assets/logo.svg" alt="HawkHacks Logo" width="35px" />
 
-					<ul className="flex w-full items-center justify-evenly gap-5">
+					<ul className="flex w-full items-center justify-evenly gap-6">
 						<li>
 							<a href="#">Home</a>
 						</li>
@@ -50,7 +52,7 @@ const Navbar = () => {
 				<div className="relative">
 					<a
 						href="/"
-						className={`tracking-widest border-2 rounded-md p-1 hover:bg-white hover:text-black transition-all duration-500 ease-in-out ${
+						className={`tracking-widest border-2 rounded-md p-2 hover:bg-white hover:text-black transition-all duration-500 ease-in-out ${
 							scrollPos < 100 ? "mr-48" : "mr-0"
 						}`}
 					>
@@ -59,7 +61,7 @@ const Navbar = () => {
 					<img
 						src="./src/assets/mlh_banner.svg"
 						alt="MLH Banner Logo"
-						className="absolute right-0 -top-5 shadow-2xl"
+						className="absolute right-0 -top-10 shadow-2xl"
 						style={{
 							opacity: bannerOpacity,
 							transition: "opacity 0.8s ease-out",
