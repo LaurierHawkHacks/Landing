f76cbaf86c80e604b4c1788ccf8be6d3500ea498
nora chams <nchamseddin@gmail.com>
2023-11-25 22:28:29 -0500

    fixed spacing

--- src/components/sections/Hero.section.tsx
+++ src/components/sections/Hero.section.tsx
@@ -34,9 +34,9 @@ const HeroSection: React.FC = () => {
     return (
         <div className="bg-midnight text-white py-16">
           <div className="text-center">
-            <h1 className="font-bold text-mint text-7xl">&lt;HawkHacks 2024/&gt;</h1>           
+            <h1 className=" mb-2 font-bold text-mint text-7xl">&lt;HawkHacks 2024/&gt;</h1>           
             <p className="pb-2 text-2xl">Wilfrid Laurier’s first large-scale global hackathon!</p>
-            <p className="pb-3 text-base font-thin [word-spacing:5px]">Waterloo, ON | April 6th - 8th | In-Person</p>
+            <p className="pb-3 text-base font-thin [word-spacing:5px]">Waterloo, ON | April 5th - 7th | In-Person</p>
             <p className="mx-5 space-x-8 text-lg font-bold [word-spacing:45px]">{countdown}</p>
             <p className="pb-3 text-base mb-2 font-thin [word-spacing:35px] -mr-2">Days Hrs Mins Secs</p>
             <button className="bg-mint text-white mb-3 px-4 py-2 rounded font-bold [letter-spacing:2px]">
