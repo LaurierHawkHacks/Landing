95276f96576c7bedd5dfd1001ef11907156c8b4c
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-12-26 18:12:05 -0500

    Add: loader and functionality

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,23 +1,42 @@
 import {
-  HeroSection,
-  InfoSection,
-  StatSection,
-  FAQSection,
-  Navbar,
-  FooterSection,
-} from "@components";
+    HeroSection,
+    InfoSection,
+    StatSection,
+    FAQSection,
+    Navbar,
+    FooterSection,
+} from '@components';
+import { useState, useEffect } from 'react';
 
 const Landing = () => {
-  return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <InfoSection />
-      <StatSection />
-      <FAQSection />
-      <FooterSection />
-    </div>
-  );
+    // State to control loader visibility
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Simulate loading (e.g., fetching data)
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000); // 3 seconds load time
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div>
+            {isLoading ? (
+                <div className="loader"></div>
+            ) : (
+                <div>
+                    <Navbar />
+                    <HeroSection />
+                    <InfoSection />
+                    <StatSection />
+                    <FAQSection />
+                    <FooterSection />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export { Landing };
