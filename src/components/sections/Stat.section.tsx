b5720b0af3fa0ade0d96253303b144259ecb57ef
AmirAgassi <amiragassi04@gmail.com>
2024-01-18 21:39:02 -0500

    Fix Stats & Footer sections hero images scaling

--- src/components/sections/Stat.section.tsx
+++ src/components/sections/Stat.section.tsx
@@ -1,15 +1,13 @@
 const StatSection = () => {
     return (
-        <>
-            <section className="relative h-screen flex items-center justify-center text-center text-[#CEDFFF] bg-midnight">
-            <img src="./src/assets/moon-and-stars.png" className="absolute inset-0 object-cover"></img>
-                <div className="flex flex-col items-center justify-center relative bottom-28 left-12 ml-96">
-                    <h2 className="drop-shadow-sm font-bold text-[58px]">650 Hackers</h2>
-                    <h2 className="drop-shadow-sm font-bold text-[58px]">208 Schools</h2>
-                    <h2 className="drop-shadow-sm font-bold text-[58px]">208 Prizes</h2>
-                </div>
-            </section>
-        </>
+        <section className="relative h-screen flex items-center justify-center text-center text-[#CEDFFF] bg-midnight">
+            <img src="./src/assets/moon-and-stars.png" className="absolute inset-0 w-full h-full object-cover" alt="Background"></img>
+            <div className="z-10 px-4 ml-64">
+                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">650 Hackers</h2>
+                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">208 Schools</h2>
+                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">208 Prizes</h2>
+            </div>
+        </section>
     );
 };
 
