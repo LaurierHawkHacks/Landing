6fdea901cd6615cf83c48cc25b2a0f28bc29063c
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-01-22 22:42:20 -0500

    fix: make footer responsive

--- src/components/sections/Footer.section.tsx
+++ src/components/sections/Footer.section.tsx
@@ -1,39 +1,39 @@
 import {
-  HeroIllustration,
-  LinkedInIcon,
-  TwitterIcon,
-  InstagramIcon,
-  DiscordIcon,
-} from "@assets";
+    HeroIllustration,
+    LinkedInIcon,
+    TwitterIcon,
+    InstagramIcon,
+    DiscordIcon,
+} from '@assets';
 
 function FooterSection() {
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <footer className="bg-midnight text-white">
-      <img 
-        className="w-full block mx-auto"
-        src={HeroIllustration} 
-        alt="Hero Illustration" 
-      />
-      <div className="mt-[-5.5rem] flex flex-col md:flex-row items-center justify-between px-4 md:px-24">
-        <a
-          href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-          target="_blank"
-          className="mb-4 md:mb-0" // Adjust margin for mobile view
-        >
-          Code of Conduct
-        </a>
-        <div className="flex space-x-7 mb-4 md:mb-0">
-          <LinkedInIcon />
-          <TwitterIcon />
-          <InstagramIcon />
-          <DiscordIcon />
-        </div>
-        <div>Copyright © {currentYear} HawkHacks</div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="bg-midnight text-white">
+            <img
+                className="w-full block mx-auto"
+                src={HeroIllustration}
+                alt="Hero Illustration"
+            />
+            <div className="bg-[#01082d] flex flex-wrap gap-x-6 items-center justify-between text-xs px-6 pb-2 sm:px-8 md:px-24">
+                <div className="md:text-base">© {currentYear} HawkHacks</div>
+                <a
+                    href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+                    target="_blank"
+                    className="text-xs md:text-base"
+                >
+                    Code of Conduct
+                </a>
+                <div className="space-x-4">
+                    <LinkedInIcon />
+                    <TwitterIcon />
+                    <InstagramIcon />
+                    <DiscordIcon />
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export { FooterSection };
