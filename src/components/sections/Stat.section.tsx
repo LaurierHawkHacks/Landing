6869642595d9695c3d272edf3755f4328d2adb9d
AmirAgassi <amiragassi04@gmail.com>
2024-01-26 15:29:54 -0500

    Fix overflowing stats

--- src/components/sections/Stat.section.tsx
+++ src/components/sections/Stat.section.tsx
@@ -2,10 +2,12 @@ const StatSection = () => {
     return (
         <section className="relative h-screen flex items-center justify-center text-center text-[#CEDFFF] bg-midnight">
             <img src="./src/assets/moon-and-stars.png" className="absolute inset-0 w-full h-full object-cover" alt="Background"></img>
-            <div className="z-10 px-4 ml-64">
-                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">650 Hackers</h2>
-                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">208 Schools</h2>
-                <h2 className="drop-shadow-sm font-bold text-5xl md:text-6xl">208 Prizes</h2>
+            <div className="z-10 px-4 ml-28">
+                <div className="flex flex-col items-center">
+                    <p className="drop-shadow-sm font-bold text-4xl sm:text-5xl md:text-6xl">650 Hackers</p>
+                    <p className="drop-shadow-sm font-bold text-4xl sm:text-5xl md:text-6xl">208 Schools</p>
+                    <p className="drop-shadow-sm font-bold text-4xl sm:text-5xl md:text-6xl">208 Prizes</p>
+                </div>
             </div>
         </section>
     );
