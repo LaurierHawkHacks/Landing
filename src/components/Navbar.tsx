39cbcacc1b9e2dca0a61435a986942c7349bad41
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-01-29 10:00:24 -0500

    feature: responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,34 @@
-import { useEffect, useState } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
+import { useLockBodyScroll } from '@uidotdev/usehooks';
+
+interface SideMenu {
+    showMenu: boolean;
+    handleClose: MouseEventHandler;
+    scrollPos: number;
+}
+
+const links = [
+    {
+        title: 'Home',
+        anchor: '#',
+    },
+    {
+        title: 'About',
+        anchor: '/about',
+    },
+    {
+        title: 'Sponsors',
+        anchor: '/sponsors',
+    },
+    {
+        title: 'FAQ',
+        anchor: '/faq',
+    },
+    {
+        title: 'Contact',
+        anchor: '/contact',
+    },
+];
 
 const useScrollPosition = () => {
     const [scrollPos, setScrollPos] = useState(0);
@@ -15,56 +45,98 @@ const useScrollPosition = () => {
     return scrollPos;
 };
 
+const LivePortalBtn = () => (
+    <a
+        href="/"
+        className={`tracking-widest border-2 rounded-md p-2 px-4 font-bold hover:bg-white hover:text-black transition-all duration-500 ease-in-out
+    }`}
+    >
+        LIVE PORTAL
+    </a>
+);
+const SideMenu = ({ showMenu, handleClose, scrollPos }: SideMenu) => {
+    useLockBodyScroll();
+
+    return (
+        <div
+            className={`fixed top-0 right-0 h-full max-w-[65%] w-full oveflow-hidden backdrop-blur-xl z-50 py-24 px-10 ${
+                showMenu ? 'block' : 'hidden'
+            }`}
+        >
+            <button
+                className={`close-btn text-white absolute ${
+                    scrollPos < 100 ? 'top-8 right-10' : 'top-5 right-10'
+                }`}
+                onClick={handleClose}
+            >
+                <img
+                    src="./src/assets/close-icon.svg"
+                    alt="click this button to close the side menu"
+                />
+            </button>
+            <ul className="flex gap-8 flex-col lg:p-0 ">
+                {links.map((link) => (
+                    <li>
+                        <a href={link.anchor}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className="live-portal-btn mt-20">
+                <LivePortalBtn />
+            </div>
+        </div>
+    );
+};
+
 const Navbar = () => {
+    const SPACE_INLINE = 10;
     const scrollPos = useScrollPosition();
     const bannerOpacity = Math.max(1 - scrollPos / 100, 0);
-    const [showMenu, setShowMenu] = useState(false);
 
+    const [showMenu, setShowMenu] = useState(false);
     const toggleMenu = () => setShowMenu(!showMenu);
 
     return (
         <nav
-            className={`flex w-full items-center gap-2  text-white fixed top-0 z-50 px-24 justify-between ${
+            className={`fixed top-0 w-full h-fit flex items-center justify-between lg:justify-normal text-white z-50 px-10 ${
                 scrollPos > 100
                     ? 'shadow-lg p-4 transition-all duration-500 ease-in-out bg-midnight'
                     : 'p-8 transition-all duration-500 ease-in-out '
-            } `}
+            }`}
         >
-            <div className="flex items-center gap-8">
+            <div className="logo lg:mr-8">
                 <img
+                    className="w-[55px]  md:w-[60px] lg:w-[50px]"
                     src="./src/assets/logo.svg"
                     alt="HawkHacks Logo"
-                    width="35px"
                 />
+            </div>
 
-                <ul className="flex w-full items-center justify-evenly gap-6">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/sponsors">Sponsors</a>
-                    </li>
-                    <li>
-                        <a href="/faq">FAQ</a>
-                    </li>
-                    <li>
-                        <a href="/contact">Contact</a>
-                    </li>
+            <div className="nav-items hidden lg:block lg:mr-auto">
+                <ul className="flex gap-8 flex-row items-center ">
+                    {links.map((link) => (
+                        <li>
+                            <a href={link.anchor}>{link.title}</a>
+                        </li>
+                    ))}
                 </ul>
             </div>
 
-            <div className="relative">
-                <a
-                    href="/"
-                    className={`tracking-widest border-2 rounded-md p-2 px-4 font-bold hover:bg-white hover:text-black transition-all duration-500 ease-in-out ${
-                        scrollPos < 100 ? 'mr-40' : 'mr-0'
-                    }`}
-                >
-                    LIVE PORTAL
-                </a>
+            <div
+                className={`live-portal-btn hidden lg:block transition-all duration-500 ease-in-out ${
+                    scrollPos < 100 ? 'lg:mr-40' : 'lg:mr-0'
+                }`}
+            >
+                <LivePortalBtn />
+            </div>
+
+            {/* banner holder is for styling purposes only */}
+            {/* problem: since banner is positioned absolute, justify-between does not affect the banner element */}
+            {/* solution: adding a div before the banner */}
+            <div className="banner-holder order-first w-[50px] lg:hidden"></div>
+            <div
+                className={`banner absolute top-0 left-${SPACE_INLINE} lg:left-auto lg:right-${SPACE_INLINE}`}
+            >
                 <a
                     id="mlh-trust-badge"
                     href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2024-season&utm_content=white"
@@ -74,14 +146,33 @@ const Navbar = () => {
                     <img
                         src="https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"
                         alt="Major League Hacking 2024 Hackathon Season"
-                        className="absolute right-0 -top-10 shadow-2xl"
+                        className="shadow-2xl w-16 lg:w-[100px]"
                         style={{
                             opacity: bannerOpacity,
-                            transition: 'opacity 0.8s ease-out',
+                            transition: 'opacity ms ease-out',
                         }}
                         width={scrollPos < 100 ? '100px' : '0px'}
                     />
                 </a>
+            </div>
+
+            <div className="menu-toggle-btn lg:hidden">
+                <button onClick={toggleMenu}>
+                    <img
+                        src="./src/assets/hamburger-icon.svg"
+                        alt="open the menu with this button"
+                    />
+                </button>
+            </div>
+
+            <div className="side-menu absolute">
+                {showMenu && (
+                    <SideMenu
+                        scrollPos={scrollPos}
+                        showMenu={showMenu}
+                        handleClose={toggleMenu}
+                    />
+                )}
             </div>
         </nav>
     );
