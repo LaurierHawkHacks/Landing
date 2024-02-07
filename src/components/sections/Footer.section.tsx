9c27c0d72201acbadbf68d6d51cc1684ce5751f3
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-02-06 19:16:55 -0500

    add: footer contents

--- src/components/sections/Footer.section.tsx
+++ src/components/sections/Footer.section.tsx
@@ -1,5 +1,36 @@
+import {
+    MailIcon,
+    LinkedInIcon,
+    InstagramIcon,
+    TwitterIcon,
+    FacebookIcon,
+    DevpostIcon,
+} from '@assets';
+
 const FooterSection = () => {
-    return <h1>footer section</h1>;
+    const currentYear = new Date().getFullYear();
+    const links = {
+        codeOfConduct: '#',
+        becomeASponsor: '#',
+    };
+
+    return (
+        <footer>
+            <div className="socials">
+                <MailIcon />
+                <LinkedInIcon />
+                <InstagramIcon />
+                <TwitterIcon />
+                <FacebookIcon />
+                <DevpostIcon />
+            </div>
+            <div className="links">
+                <a href={links.codeOfConduct}>Code of Conduct</a>
+                <a href={links.becomeASponsor}>Become a Sponsor</a>
+            </div>
+            <p>Copyright © {currentYear} HawkHacks</p>
+        </footer>
+    );
 };
 
 export { FooterSection };
