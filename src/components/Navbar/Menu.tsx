236955bd649a85a2e99a28cfb22e1e19cf92c667
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-02-13 17:42:49 -0500

    refactor: guard clause

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,5 +1,3 @@
-import { useLockBodyScroll } from '@uidotdev/usehooks';
-
 import { NavItems } from '@components';
 
 interface MenuProp {
@@ -8,14 +6,10 @@ interface MenuProp {
 }
 
 const Menu: React.FC<MenuProp> = ({ showMenu, hideMenu }) => {
-    // useLockBodyScroll();
+    if (!showMenu) return;
 
     return (
-        <div
-            className={`oveflow-hidden fixed right-0 top-0 z-50 h-full w-full max-w-[65%] border px-10 py-24 backdrop-blur-xl ${
-                showMenu ? 'block' : 'hidden'
-            }`}
-        >
+        <div className="oveflow-hidden fixed right-0 top-0 z-50 h-full w-full max-w-[65%] border px-10 py-24 backdrop-blur-xl">
             <NavItems isHorizontal={false} handleClick={hideMenu} />
 
             <div className="portal-btn mt-20">
