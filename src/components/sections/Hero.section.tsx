503b0c2340df1c5e9a4118e78471fa9e919a56a1
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-02-20 14:59:53 -0500

    update: hero section

--- src/components/sections/Hero.section.tsx
+++ src/components/sections/Hero.section.tsx
@@ -1,29 +1,23 @@
+import { HeroBackgroundMobile, HeroBackgroundDesktop } from '@assets';
+
 const HeroSection = () => {
     return (
         <div className="relative isolate">
             <div className="-z-10">
-                <img
-                    className="sm:hidden"
-                    src="src/assets/hero/smaller.svg"
-                    alt=""
-                />
-                <img
-                    className="hidden sm:block"
-                    src="src/assets/hero/hero.svg"
-                    alt=""
-                />
+                <HeroBackgroundMobile className={'block sm:hidden'} />
+                <HeroBackgroundDesktop className={'hidden sm:block'} />
             </div>
-            <div className="relative -mt-72 space-y-4 text-center sm:hidden ">
+            <div className="relative -mt-72 space-y-4 text-center sm:absolute sm:left-0 sm:top-0 sm:mt-0 sm:p-10 lg:p-14 sm:space-y-2 sm:text-left lg:space-y-7">
                 <img
-                    className="mx-auto aspect-square w-24 md:w-36 lg:w-[18.75rem]"
+                    className="mx-auto aspect-square w-24 sm:mx-0 lg:w-28 xl:w-32"
                     src="src/assets/logo.svg"
                     alt="Hawkhack logo"
                 />
-                <h1 className="hero-title-gradient text-4xl font-extrabold md:text-5.5xl lg:text-8.5xl">
+                <h1 className="hero-title-gradient text-4xl font-extrabold lg:text-5.5xl xl:text-7xl">
                     HawkHack 2024
                 </h1>
-                <p className="text-2xl font-bold">April 5th - 7th</p>
-                <p className="text-2xl font-bold">In Person</p>
+                <p className="text-2xl font-bold sm:hidden">April 5th - 7th</p>
+                <p className="text-2xl font-bold sm:hidden">In Person</p>
             </div>
         </div>
     );
