473c45b920b064184fe4e30f6ec56506155242ce
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-21 21:43:36 -0500

    Update: semi-working images

--- src/components/sections/About.section.tsx
+++ src/components/sections/About.section.tsx
@@ -1,10 +1,24 @@
-import billboard from '../../assets/svg/billboard.svg';
-// import billboard from '../../assets/about/billboard.svg';
+// import AboutBillboard from '../../assets/about/AboutBillboard.svg';
+// import AboutWifi from '../../assets/about/AboutWifi.svg';
+// import AboutDetails from '../../assets/about/AboutDetails.svg';
+import AboutAirBalloon from '../../assets/about/AboutAirBalloon.svg';
+import AboutSection from '../../assets/about/AboutSection.svg';
 
 const About: React.FC = () => {
     return (
-        <div>
-            <img src={billboard} alt="Billboard" className="" />
+        <div className="relative h-screen w-screen">
+            <div className="relative left-0 top-64 ml-10 h-auto w-[128%] overflow-hidden">
+                <img
+                    src={AboutSection}
+                    alt="Billboard"
+                    className="block" // Adjust classes as needed
+                />
+            </div>
+            <img
+                src={AboutAirBalloon}
+                alt="Billboard"
+                className="absolute left-[50%] top-[140%] w-[20%]"
+            />
         </div>
     );
 };
