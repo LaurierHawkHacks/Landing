4d5f2dcea064e4f098e612eae0e0116a2209f0b7
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-02-22 21:28:40 -0500

    fix: adding top spacing at the wrapper to prevent navbar overlapping

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '@components';
 
 const Landing: React.FC = () => {
     return (
-        <div>
+        <div className="pt-36">
             <Navbar />
             <FooterSection />
         </div>
