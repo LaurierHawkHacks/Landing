889dc9c397b6059bc7d8eec96817048c3c1dc283
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-03-15 16:46:42 -0400

    Update HeroStats.section.tsx

--- src/components/sections/HeroStats.section.tsx
+++ src/components/sections/HeroStats.section.tsx
@@ -42,7 +42,7 @@ const HeroStatSection = () => {
         <div className="relative">
             <section
                 ref={heroRef}
-                className="hero-section stacked place-items-end-center absolute sm:place-items-start"
+                className="hero-section stacked place-items-end-center absolute bg-peachPuff sm:place-items-start"
             >
                 <img src={HeroAboutDesktop} />
                 {/* <HeroBackgroundMobile className="media block pb-16 sm:hidden xl:translate-y-20" /> */}
@@ -71,6 +71,36 @@ const HeroStatSection = () => {
                         </p>
                     </div>
                 </div>
+                <div className="absolute left-1/2 top-[70%] w-2/3 -translate-x-1/2 -translate-y-1/2 transform text-white ">
+                    <h1 className="mb-5 text-center text-4.5xl uppercase drop-shadow-md lg:text-6.5xl xl:text-7.5xl">
+                        About Hawkhacks
+                    </h1>
+                    <div className="space-y-10 text-center">
+                        <p>
+                            HawkHacks came out of a desire to give everyone an
+                            equal opportunity to get into tech, whether that be
+                            programming, networking, researching, learning, or
+                            teaching.
+                        </p>
+                        <p>
+                            Join hundreds of students across Canada (and across
+                            the world) in a 36 hour period of exploration,
+                            creativity, and learning!
+                        </p>
+                        <p>
+                            Remember, you don’t have to be a pro to participate
+                            - show up with ten years or ten minutes of
+                            experience (oh yeah, and a great attitute too!)
+                        </p>
+                    </div>
+                </div>
+                {/* Hawk Air Balloon */}
+                <img
+                    src={AboutAirBalloon}
+                    alt=""
+                    className="absolute left-[70%] top-1/3 hidden w-[20%] sm:top-1/4 md:block"
+                    style={{ transform: `translateY(${translateY}px)` }} // Move balloon with scroll
+                />
             </section>
 
             {/* about section */}
@@ -90,37 +120,6 @@ const HeroStatSection = () => {
                     {/*     /> */}
                     {/* </div> */}
                     {/* About HawkHacks Content */}
-                    <div className="absolute left-1/2 top-[70%] w-2/3 -translate-x-1/2 -translate-y-1/2 transform text-white ">
-                        <h1 className="mb-5 text-center text-4.5xl uppercase drop-shadow-md lg:text-6.5xl xl:text-7.5xl">
-                            About Hawkhacks
-                        </h1>
-                        <div className="space-y-10 text-center">
-                            <p>
-                                HawkHacks came out of a desire to give everyone
-                                an equal opportunity to get into tech, whether
-                                that be programming, networking, researching,
-                                learning, or teaching.
-                            </p>
-                            <p>
-                                Join hundreds of students across Canada (and
-                                across the world) in a 36 hour period of
-                                exploration, creativity, and learning!
-                            </p>
-                            <p>
-                                Remember, you don’t have to be a pro to
-                                participate - show up with ten years or ten
-                                minutes of experience (oh yeah, and a great
-                                attitute too!)
-                            </p>
-                        </div>
-                    </div>
-                    {/* Hawk Air Balloon */}
-                    <img
-                        src={AboutAirBalloon}
-                        alt=""
-                        className="absolute left-[70%] top-1/3 hidden w-[20%] sm:top-1/4 md:block"
-                        style={{ transform: `translateY(${translateY}px)` }} // Move balloon with scroll
-                    />
                 </div>
             </section>
         </div>
