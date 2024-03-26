29e05a9aaf51ec96411faa32c38e728018318f4d
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-26 07:12:12 -0400

    Update navbar padding

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -10,11 +10,7 @@ import {
 
 const Landing: React.FC = () => {
     return (
-        // padding calculation
-        // logo in navbar height = 3.5rem
-        // navbar adds top and bottom padding of 2rem
-        // total top padding = 3.5 + 4 = 7.5rem
-        <div className="pt-[7.5rem]">
+        <div className="pt-[5rem]">
             <Navbar />
             <HeroStatSection />
             <SponsorFAQSection />
