90438051cbab9cd9c84cb6d49d3e232e044eacb0
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-26 10:40:30 -0400

    Update footer

--- src/components/sections/Footer.section.tsx
+++ src/components/sections/Footer.section.tsx
@@ -1,33 +1,21 @@
-import {
-    MailIcon,
-    LinkedInIcon,
-    InstagramIcon,
-    TwitterIcon,
-    FacebookIcon,
-    DevpostIcon,
-} from '@assets';
+import { SocialIcons } from "@components";
 
 const FooterSection = () => {
     return (
-        <footer className="p-4 sm:text-center">
-            <div className="sm:space-x-2 md:space-x-3">
-                <MailIcon />
-                <LinkedInIcon />
-                <InstagramIcon />
-                <TwitterIcon />
-                <FacebookIcon />
-                <DevpostIcon />
+        <footer className="p-4 text-center bg-darkViolet py-20">
+            <div className="flex justify-center items-center pb-6">
+                <SocialIcons />
             </div>
-            <div className="links flex flex-col sm:flex-row sm:justify-center sm:gap-3 ">
+            <div className="links flex flex-col items-center gap-3 sm:flex-row sm:justify-center">
                 <a
-                    className="underline hover:text-[#0FA3B1] focus:text-[#1D7882]"
-                    href="#"
+                    className="underline text-weirdViolet hover:text-[#0FA3B1] focus:text-[#1D7882] mb-2 sm:mb-0" // Added mb-2 for spacing between links on mobile
+                    href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
                 >
                     Code of Conduct
                 </a>
                 <a
-                    className="underline hover:text-[#0FA3B1] focus:text-[#1D7882]"
-                    href="#"
+                    className="underline text-weirdViolet hover:text-[#0FA3B1] focus:text-[#1D7882]"
+                    href=""
                 >
                     Become a Sponsor
                 </a>
