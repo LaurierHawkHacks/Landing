4dcba70aff25e56940e5fa8f2a009620215b8faf
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-26 06:37:10 -0400

    Update FAQ information

--- src/components/sections/SponsorFAQ.section.tsx
+++ src/components/sections/SponsorFAQ.section.tsx
@@ -26,32 +26,36 @@ import {
 const SponsorFAQSection = () => {
     const faqData = [
         {
-            question: "So, what exactly is a hackathon?",
-            answer: "Our product is...",
+            question: "🤔 So, what exactly is a hackathon?",
+            answer: "A hackathon is made up of two sections - a social coding event where programmers, designers, and developers collaborate in teams to solve a problem.\\n You can either create a software-based project that falls under a category hack, or make whatever your heart desires as a general hack. \\n The second section is networking events, seminars, and presentations that happen throughout the hackathon conducted by industry and academic professionals - if you've been putting off learning that one programming language for a while, now might be the time to learn!",
         },
         { 
-            question: "Do I need a team?", 
-            answer: "Our product is..." 
+            question: "🕒 When and where is HawkHacks happening?", 
+            answer: "HawkHacks will take place for 36 hours, starting on some day idk, Some day @ 6PM EDT all the way till Someday @ 2PM EDT.  \\n HawkHacks is a fully in-person event, meaning everything will be hosted and managed at Wilfrid Laurier University!"
         },
         {
-            question: "When and where is HawkHacks happening?",
-            answer: "Our product is...",
+            question: "💰 How much does it cost?",
+            answer: "Nothing! Free! Nada! Due to our amazing sponsors and support systems, attending and participating at HawkHacks is absolutely free!",
         },
         { 
-            question: "Are there any prizes?", 
-            answer: "Our product is..." 
+            question: "💻 Who can attend? Do I need to be a skilled leet programmer?", 
+            answer: "Anybody from someone who doesn’t know what a computer is, to someone who makes computers, can attend the hackathon!\\nYou can either create a piece of software in 36-hours, attend the workshops and seminars, or do a bit of both - everything is up to you!" 
         },
         { 
-            question: "How much does it cost?", 
-            answer: "Our product is..." 
+            question: "👪 Do I need a team?", 
+            answer: " You can work alone, or all the way up to a team of 2, 3, or 4 people! \\nWe highly encourage working in a team! \\nIf you don't have friends who are attending, you can find a team in our Discord OR attend our team finding event before the hackathon!." 
         },
         {
-            question: "Who can attend? Do I need to be a skilled leet programmer?",
-            answer: "Our product is...",
+            question: "🐦 Why did y'all choose a bird as the mascot?",
+            answer: "Midas, the Golden Hawk, is our school mascot!",
         },
         {
-            question: "This is a cool FAQ but I still have questions!",
-            answer: "Our product is...",
+            question: "🏆 Are there any prizes?",
+            answer: "We have a tonne of different prizes!\\nWe have our general prizes for any submission, as well as prizes for each specific category hack - some of these prizes are from sponsors, and others are directly from us!\\nWe also do random giveaways and prizes throughout the event!\\nWe might have random mini-game events, or even random giveaways whenever Nausher is feeling nice :)"
+        },
+        {
+          question: "📧 This is a cool FAQ section, but I still have questions!",
+          answer: "No worries! Reach out to us at hello@hawkhacks.ca or our social media, and we'll get back to you as soon as our social media and email person wakes up!",
         },
     ];
 
