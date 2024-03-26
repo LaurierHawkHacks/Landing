bdd36bb5deda6d2b5761a8b6c2e9701fbdf610d5
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-03-26 19:17:15 -0400

    Implement LoadingAnimation

---
+++ src/components/LoadingAnimation.tsx
@@ -0,0 +1,13 @@
+const LoadingAnimation: React.FC = () => {
+    return (
+        <div className="loading-container flex h-screen items-center justify-center">
+            <img
+                className="h-14 w-14 animate-ping "
+                src="./src/assets/logo.svg"
+                alt="Loading Animation"
+            />
+        </div>
+    );
+};
+
+export { LoadingAnimation };
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
     Navbar,
     HeroStatSection,
@@ -6,11 +7,26 @@ import {
     SponsorFAQSection,
     FooterSection,
     ScrollButton,
+    LoadingAnimation,
 } from '@components';
 
 const Landing: React.FC = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Set a delay for the loading animation
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000); // 3000ms = 3 seconds
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return <LoadingAnimation />;
+    }
     return (
-        <div className="pt-[5rem]">
+        <div className="pt-[5rem] transition ease-in">
             <Navbar />
             <HeroStatSection />
             <SponsorFAQSection />
