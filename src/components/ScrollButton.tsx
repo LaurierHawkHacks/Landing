6267334b2d981c3da9fb9d2ef6e474a16be87590
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-27 11:10:36 -0400

    Add offset to scroll to top

---
+++ src/components/ScrollButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import { IoMdArrowUp } from "react-icons/io";
+
+const ScrollButton: React.FC = () => {
+    return (
+        <div className="fixed bottom-6 right-8 z-50">
+            <Link
+                to="top"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+                offset={-75}
+            >
+                <div className="w-12 h-12 bg-deepMarine rounded-full shadow-lg flex items-center justify-center">
+                    <IoMdArrowUp className="text-white" />
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export { ScrollButton };
