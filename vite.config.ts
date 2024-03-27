54e76686cdf34c39549e281581a41974b1503eab
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-27 13:15:14 -0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
+    base: isProduction ? '/Landing/' : '/',
     plugins: [react()],
     resolve: {
         alias: {
