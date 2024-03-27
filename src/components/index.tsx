c1a68da23ed052321764fbda7ac2fd2d27fa9589
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-27 11:10:26 -0400

    Change file name and update export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -9,7 +9,7 @@ export { NavItems } from './Navbar/NavItems';
 export { Navbar } from './Navbar/Navbar';
 export { Menu } from './Navbar/Menu';
 export { LoadingAnimation } from './LoadingAnimation';
-export { ScrollButton } from './Scroll.button';
+export { ScrollButton } from './ScrollButton';
 export { SocialIcons } from './SocialIcons';
 
 /*
