59662fafeb73df30fee2ebc8e9415bfe8267dd2e
Jc <46619361+juancwu@users.noreply.github.com>
2024-03-30 00:24:59 -0400

    Update src/components/Navbar/Menu.tsx

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -13,7 +13,7 @@ const Menu: React.FC<MenuProp> = ({ showMenu, hideMenu }) => {
         <div className="oveflow-hidden fixed right-0 top-0 z-50 h-full w-full max-w-[65%] border px-10 py-24 backdrop-blur-xl">
             <NavItems isHorizontal={false} handleClick={hideMenu} />
             <Button className="mt-20 block lg:hidden">
-                <Link to="./comingsoon">Application Portal</Link>
+                <Link to="/comingsoon">Application Portal</Link>
             </Button>
         </div>
     );
