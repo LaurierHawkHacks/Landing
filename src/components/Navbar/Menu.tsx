589392d3b7b291c7b9882816a9d1d4e9ba5c2c7c
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-03-30 20:43:26 -0400

    fix sidebar styling

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -10,10 +10,10 @@ const Menu: React.FC<MenuProp> = ({ showMenu, hideMenu }) => {
     if (!showMenu) return;
 
     return (
-        <div className="oveflow-hidden fixed right-0 top-0 z-50 h-screen max-w-[65%] border border-white bg-white/20 px-10 py-24 backdrop-blur-lg">
+        <div className="oveflow-hidden fixed right-0 top-0 z-50 h-screen max-w-[65%] w-full border border-white bg-white/20 px-10 py-24 backdrop-blur-lg">
             <NavItems isHorizontal={false} handleClick={hideMenu} />
-            <Button className="mt-20 block p-0">
-                <Link className="px-4 py-2" to="/comingsoon">
+            <Button className="mt-20 block">
+                <Link className="text-sm" to="/comingsoon">
                     Application Portal
                 </Link>
             </Button>
