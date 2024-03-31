1e4d82fc6839aa5923f8b229a668f1a23e76d809
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-30 22:41:38 -0400

    Update footer section

--- src/components/sections/Footer.section.tsx
+++ src/components/sections/Footer.section.tsx
@@ -2,25 +2,29 @@ import { SocialIcons } from "@components";
 
 const FooterSection = () => {
     return (
-        <footer className="px-4 text-center bg-darkViolet py-20">
-            <div className="flex justify-center items-center pb-6">
-                <SocialIcons color="#C8B8F6"/>
+        <footer className="px-4 text-center bg-footerBg py-20">
+            <div className="flex justify-center items-center pb-10">
+                <SocialIcons color="white"/>
             </div>
-            <div className="links flex flex-col items-center gap-3 sm:flex-row sm:justify-center">
+            <div className="links flex flex-col items-center gap-3 sm:flex-row sm:justify-center pb-5">
                 <a
-                    className="underline text-weirdViolet hover:text-[#0FA3B1] focus:text-[#1D7882] mb-2 sm:mb-0" // Added mb-2 for spacing between links on mobile
+                    className="underline text-white hover:text-[#0FA3B1] focus:text-[#1D7882] mb-2 sm:mb-0"
                     href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+                    target="_blank"
+                    rel="noreferrer"
                 >
                     Code of Conduct
                 </a>
                 <a
-                    className="underline text-weirdViolet hover:text-[#0FA3B1] focus:text-[#1D7882]"
-                    href=""
+                    className="underline text-white hover:text-[#0FA3B1] focus:text-[#1D7882]"
+                    href="https://hawkhacks.ca/sponsorships.pdf"
+                    target="_blank"
+                    rel="noreferrer"
                 >
                     Become a Sponsor
                 </a>
             </div>
-            <p className="mt-4 lg:mt-8 2xl:text-2xl text-weirdViolet">
+            <p className="mt-4 lg:mt-8 2xl:text-2xl text-white">
                 Copyright © 2024 HawkHacks
             </p>
         </footer>
