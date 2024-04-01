9d2e4b8831817198b3a7faa0d872927be5509e3b
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-04-01 02:20:12 -0400

    Update router

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ErrorPage, Landing } from '@pages'
+import { ComingSoon, ErrorPage, Landing } from '@pages'
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="*" element={<ErrorPage />} />
+            <Route path="/coming-soon" element={<ComingSoon />} />
         </Routes>
     </BrowserRouter>
 );
