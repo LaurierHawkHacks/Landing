9647b02dfab4bdf79c327968a755da06af35205a
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-04-07 05:52:38 -0400

    Update asset exports

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -42,91 +42,70 @@ export { ArrowRightIcon, BirdParts, BottomBorder, Hawk, MiddleBody, TopBorder };
  * Sponsor & Partner logos PNGs
  *
  */
-import OnePassword from './logos/1password.png';
-import Assembly from './logos/assembly.png';
 import Balsamiq from './logos/balsamiq.png';
-import CCubed from './logos/ccubed.png';
-import Dcl from './logos/dcl.png';
-import DigitalOcean from './logos/digitalocean.png';
-import Echo3D from './logos/echo3d.png';
-import Fdm from './logos/fdm.png';
-import Fossa from './logos/fossa.png';
-import Lcs from './logos/lcs.png';
-import LeadingLeaders from './logos/leadingleaders.png';
-import Mlh from './logos/mlh.png';
-import Ollon from './logos/ollon.png';
-import Roomiez from './logos/roomiez.png';
-import Soc from './logos/soc.png';
-import StudentsUnion from './logos/studentsunion.png';
-import Taskade from './logos/taskade.png';
-import Veritas from './logos/veritas.png';
-
-import Certopus from './logos/Certopus.png';
-import Cisco from './logos/Cisco.png';
-import DeFiBlocks from './logos/DeFiBlocks.png';
-import Dominos from './logos/Dominos.png';
+import Certopus from './logos/certopus.png';
+import Cisco from './logos/cisco.png';
+import DeFiBlocks from './logos/defiblocks.png';
 import Distributive from './logos/distributive.png';
+import Dominos from './logos/dominos.png';
+import Echo3D from './logos/echo3d.png';
 import Ennios from './logos/ennios.png';
-import Fantuan from './logos/Fantuan.png';
-import IndoFood from './logos/IndoFood.png';
-import InterviewBuddy from './logos/InterviewBuddy.png';
+import Fantuan from './logos/fantuan.png';
+import Fintech from './logos/fintech.png';
+import Fossa from './logos/fossa.png';
+import IndoFood from './logos/indofood.png';
+import InterviewBuddy from './logos/interviewbuddy.png';
 import InterviewCake from './logos/interviewcake.png';
-import JDoodle from './logos/JDoodle.png';
-import Near from './logos/NEAR.png';
-import Nibiru from './logos/Nibiru.png';
-import Roku from './logos/Roku.png';
-import SmokesPoutinerie from './logos/Smokes-Poutinerie.png';
-import SweetLouLogo from './logos/SweetLouLogo.png';
-import Vectara from './logos/Vectara.png';
-import Verbwire from './logos/Verbwire.png';
-import Fintech from './logos/WLU-Fintech.png';
-import Wolfram from './logos/Wolfram.png';
-import Solana from './logos/Solana.png';
+import JDoodle from './logos/jdoodle.png';
+import Lcs from './logos/lcs.png';
+import Mlh from './logos/mlh.png';
+import Near from './logos/near.png';
+import Nibiru from './logos/nibiru.png';
+import OnePassword from './logos/1password.png';
+import Roku from './logos/roku.png';
 import Rosenfeld from './logos/rosenfeld.png';
-import StreamYard from './logos/StreamYard.png';
+import SmokesPoutinerie from './logos/smokes.png';
+import Solana from './logos/solana.png';
+import StreamYard from './logos/streamyard.png';
+import StudentsUnion from './logos/studentsunion.png';
+import SweetLous from './logos/sweetlous.png';
+import Taskade from './logos/taskade.png';
+import Vectara from './logos/vectara.png';
+import Verbwire from './logos/verbwire.png';
+import Wolfram from './logos/wolfram.png';
 
 export {
-    OnePassword,
-    Assembly,
     Balsamiq,
-    CCubed,
-    Dcl,
-    DigitalOcean,
-    Dominos,
-    Distributive,
-    Echo3D,
-    Fdm,
-    Fossa,
-    Lcs,
-    LeadingLeaders,
-    Mlh,
-    Ollon,
-    Roomiez,
-    Soc,
-    StudentsUnion,
-    Taskade,
-    Veritas,
     Certopus,
     Cisco,
     DeFiBlocks,
+    Distributive,
+    Dominos,
+    Echo3D,
     Ennios,
     Fantuan,
+    Fintech,
+    Fossa,
     IndoFood,
     InterviewBuddy,
     InterviewCake,
     JDoodle,
+    Lcs,
+    Mlh,
     Near,
     Nibiru,
+    OnePassword,
     Roku,
+    Rosenfeld,
     SmokesPoutinerie,
-    SweetLouLogo,
+    Solana,
+    StreamYard,
+    StudentsUnion,
+    SweetLous,
+    Taskade,
     Vectara,
     Verbwire,
-    Fintech,
     Wolfram,
-    Solana,
-    Rosenfeld,
-    StreamYard,
 };
 
 /*
