2a2e0dce89e81d6ea258bd439c70ca4180612125
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-04-13 09:58:42 -0400

    Update formatting

--- src/components/sections/data/faqData.ts
+++ src/components/sections/data/faqData.ts
@@ -4,7 +4,7 @@ const faqData = [
         content: [
             {
                 question: '🤔 What is a Hackathon?',
-                answer: 'A hackathon consists of two “parts” - a social coding event where teams of programmers, designers, and developers collaborate to solve a problem. You can either create a software-based project that falls under a category hack or make whatever your heart desires as a general hack.\\nThe second section involves networking events, seminars, and presentations conducted by industry and academic professionals. These mini-events are scheduled throughout the hackathon - they’re completely optional, but are great ways to take a break from your hacking, and to even learn a new skill!',
+                answer: 'A hackathon consists of two “parts” - a social coding event where teams of programmers, designers, and developers collaborate to solve a problem. You can either create a software-based project that falls under a category hack or make whatever your heart desires as a general hack.\\nThe second section involves networking events, seminars, and presentations conducted by industry and academic professionals. These mini-events are scheduled throughout the hackathon - they\'re completely optional, but are great ways to take a break from your hacking, and to even learn a new skill!',
             },
             {
                 question:
@@ -18,17 +18,18 @@ const faqData = [
             {
                 question:
                     '📧 This is a cool FAQ section, but I still have a question!',
-                answer: 'No worries! Reach out to us via email at <a href="mailto:hello@hawkhacks.ca">hello@hawkhacks.ca</a> or on any of <a href="https://linktr.ee/hawkhacks">our socials</a>, and we’ll get back to you as soon as our social media person wakes up!',
+                answer: 'No worries! Reach out to us via email at <a href="mailto:hello@hawkhacks.ca">hello@hawkhacks.ca</a> or on any of <a href="https://linktr.ee/hawkhacks">our socials</a>, and we\'ll get back to you as soon as our social media person wakes up!',
             },
         ],
     },
+
     {
         section: 'Eligibility',
         content: [
             {
                 question:
                     '🔑 Do I need to know how to code like some sort of crazy hacker?',
-                answer: 'Anybody can attend HawkHacks! From someone who doesn’t know what a computer is, to someone who makes computers - everyone’s welcome!\\nYou can either create a piece of software in 36 hours, attend the workshops and seminars, or do a bit of both - all up to you!',
+                answer: 'Anybody can attend HawkHacks! From someone who doesn\'t know what a computer is, to someone who makes computers - everyone\'s welcome!\\nYou can either create a piece of software in 36 hours, attend the workshops and seminars, or do a bit of both - all up to you!',
             },
             {
                 question: '📅 When can I apply',
@@ -36,11 +37,11 @@ const faqData = [
             },
             {
                 question: '🎓 Am I eligible to participate?',
-                answer: 'As long as you’re over the age of 13, and are either a high-schooler, a post-secondary student (i.e. university or college), or a recent graduate, you are eligible to participate.',
+                answer: 'As long as you\'re over the age of 13, and are either a high-schooler, a post-secondary student (i.e. university or college), or a recent graduate, you are eligible to participate.',
             },
             {
                 question: '👫 Do I need a team?',
-                answer: 'You can work alone, or up to a team of 4 people, but we strongly encourage working with a team! If you’re looking for a team, check out our #looking-for-team <a href="https://discord.com/invite/GxwvFEn9TB">Discord channel</a> OR during our in-person team-building event.',
+                answer: 'You can work alone, or up to a team of 4 people, but we strongly encourage working with a team! \\nIf you\'re looking for a team, check out our #looking-for-team <a href="https://discord.com/invite/GxwvFEn9TB">Discord channel</a> OR during our in-person team-building event.',
             },
         ],
     },
@@ -53,22 +54,23 @@ const faqData = [
                 answer: 'At a minimum, you should have a device to code on (ideally a laptop), a charger (for your phone and laptop), and a piece of student or government ID. We also recommend bringing stuff to stay comfy, such as a blanket, pillow, and an oversized hoodie.\\nKeep in mind that this is an overnight event - you may want to consider hygiene products as well!',
             },
             {
-                question: '🔎 I’m lost! How do I find the HawkHacks building?',
-                answer: 'HawkHacks is being hosted at the Lazaridis School of Business and Economics, which is located in Waterloo, Ontario, at Wilfrid Laurier University’s main campus. Google Map directions can be <a href="https://google.com/maps/dir//Lazaridis+School+of+Business+and+Economics,+64+University+Ave+W,+Waterloo,+ON+N2L+3C7/@43.4750664,-80.6118487,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x882bf3f62c12347f:0x100f39a182234b30!2m2!1d-80.529448!2d43.475096?entry=ttu">found here</a>.',
+                question: '🔎 I\'m lost! How do I find the HawkHacks building?',
+                answer: 'HawkHacks is being hosted at the Lazaridis School of Business and Economics, which is located in Waterloo, Ontario, at Wilfrid Laurier University\'s main campus. Google Map directions can be <a href="https://google.com/maps/dir//Lazaridis+School+of+Business+and+Economics,+64+University+Ave+W,+Waterloo,+ON+N2L+3C7/@43.4750664,-80.6118487,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x882bf3f62c12347f:0x100f39a182234b30!2m2!1d-80.529448!2d43.475096?entry=ttu">found here</a>.',
             },
             {
                 question:
-                    '🚗 I’ll be commuting - do you guys cover transportation costs? How can I get to Waterloo?',
-                answer: 'Unfortunately, we’re unable to cover transportation costs. There are many ways to commute - GO Bus, GO Train, VIA Rail, and Flixbus!\\nIf those options aren’t feasible, we also have a #rideshare channel on our <a href="https://discord.com/invite/GxwvFEn9TB">Discord server</a> where people can coordinate carpools!',
+                    '🚗 I\'ll be commuting - do you guys cover transportation costs? How can I get to Waterloo?',
+                answer: 'Unfortunately, we\'re unable to cover transportation costs. There are many ways to commute - GO Bus, GO Train, VIA Rail, and Flixbus!\\nIf those options aren\'t feasible, we also have a #rideshare channel on our <a href="https://discord.com/invite/GxwvFEn9TB">Discord server</a> where people can coordinate carpools!',
             },
         ],
     },
+
     {
         section: 'Health & Safety',
         content: [
             {
                 question: '😷 What are your COVID-19 regulations?',
-                answer: 'At this time, HawkHacks does not have a COVID-19 policy in effect and won’t be enforcing any COVID-related rules or restrictions. We recommend you do your due diligence when it comes to your health, and take precautions as necessary. We will have a small amount of hygiene products, such as hand sanitizers, available at the event.\\nWe ask that if you are feeling sick or unwell, to not attend the event for the respect and safety of everyone else at the event.',
+                answer: 'At this time, HawkHacks does not have a COVID-19 policy in effect and won\'t be enforcing any COVID-related rules or restrictions. We recommend you do your due diligence when it comes to your health, and take precautions as necessary. We will have a small amount of hygiene products, such as hand sanitizers, available at the event.\\nWe ask that if you are feeling sick or unwell, to not attend the event for the respect and safety of everyone else at the event.',
             },
         ],
     },
