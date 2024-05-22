c5d81e2782055cade15f2df15a77dcb292079426
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-22 17:40:37 -0400

    Remove my dumdum import

--- src/components/sections/HeroStats.section.tsx
+++ src/components/sections/HeroStats.section.tsx
@@ -7,7 +7,6 @@ import {
 } from '@assets';
 
 import { Button } from '@components';
-import { Link } from 'react-router-dom';
 
 const HeroStatSection: React.FC = () => {
     const [translateY, setTranslateY] = useState(0);
